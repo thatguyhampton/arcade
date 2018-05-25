50fbb7a5f595f56c0bcbd6cd7abdb72b918ba63e
Shawn Hampton <thatguyhampton@gmail.com>
2018-05-25 14:34:53 -0700

    navigation added

--- App.js
+++ App.js
@@ -1,36 +1,8 @@
-import React from 'react';
-import { AppRegistry, StyleSheet, StatusBar } from 'react-native';
-import { GameEngine } from 'react-native-game-engine';
-import { Finger } from './src/renderers';
-import { MoveFinger } from './src/systems';
+import { createStackNavigator } from 'react-navigation';
+import { HomeScreen } from './src/homeScreen';
+import { SnakeScreen } from './src/snakeScreen';
 
-export default class App extends React.PureComponent {
-  render() {
-    return (
-      <GameEngine
-        style={styles.container}
-        systems={[MoveFinger]}
-        entities={{
-          1: { position: [40, 200], renderer: <Finger />},
-          2: { position: [100, 200], renderer: <Finger />},
-          3: { position: [160, 200], renderer: <Finger />},
-          4: { position: [220, 200], renderer: <Finger />}
-        }}>
-        <StatusBar hidden={true} />
-      </GameEngine>
-      // <View style={styles.container}>
-      //   <Text>The Arcade</Text>
-      //   <Text>Open up App.js to start working on your app!</Text>
-      //   <Text>Changes you make will automatically reload.</Text>
-      //   <Text>Shake your phone to open the developer menu.</Text>
-      // </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
+export default createStackNavigator({
+  Home: { screen: HomeScreen },
+  Snake: { screen: SnakeScreen },
 });
