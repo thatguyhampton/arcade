c49005f5530dbe45b62404a923d67a0180c86a85
Shawn Hampton <thatguyhampton@gmail.com>
2018-05-29 22:30:24 -0700

    web games

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { createStackNavigator } from 'react-navigation';
 import { HomeScreen } from './src/homeScreen';
 import { SnakeScreen } from './src/snakeScreen';
+import { TetrisScreen } from './src/tetrisScreen';
 
 export default createStackNavigator({
   Home: { screen: HomeScreen },
   Snake: { screen: SnakeScreen },
+  Tetris: { screen: TetrisScreen }
 });
