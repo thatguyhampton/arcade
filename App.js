7003ef0b7507af1cc357d1054949cfc03c4750f8
Shawn Hampton <thatguyhampton@gmail.com>
2018-06-13 22:43:36 -0700

    able to build actual android app

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { createStackNavigator } from 'react-navigation';
 import { HomeScreen } from './src/homeScreen';
 import { SnakeScreen } from './src/snakeScreen';
 import { TetrisScreen } from './src/tetrisScreen';
+import { MontactScreen } from './src/montactScreen';
+import { TriangleScreen } from './src/triangleScreen';
 
 export default createStackNavigator({
   Home: { screen: HomeScreen },
   Snake: { screen: SnakeScreen },
-  Tetris: { screen: TetrisScreen }
+  Tetris: { screen: TetrisScreen },
+  Montact: { screen: MontactScreen },
+  Triangle: { screen: TriangleScreen }
 });
